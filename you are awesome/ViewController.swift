9af3a7cde3028790bf0f780fc3516a8ef31a4909
Timo Bell <timo@andre-bell.de>
2022-01-01 16:48:16 +0100

    Initial Commit

---
+++ you are awesome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  you are awesome
+//
+//  Created by Timo Bell on 01.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
